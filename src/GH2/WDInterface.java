84cbac546e7002a5c5288a177433a9f80a4c93ad
RazielEL <amp91@pm.me>
2022-03-21 20:34:50 -0400

    done

--- src/GH2/WDInterface.java
+++ src/GH2/WDInterface.java
@@ -1,70 +1,114 @@
 package GH2;
 
-interface WebDriver{
-
-
-    void open();
-    void close();
-    String getTitle();
-}
-
-interface RemoteWebDriver extends WebDriver {
+interface RemoteWebDriver{
     void navigate();
 }
-
-interface TakesScreenshot extends RemoteWebDriver{
+interface TakesScreenshot {
     void getScreenshot();
 }
 
-class ChromeDriver implements TakesScreenshot {
+interface WebDriver extends RemoteWebDriver, TakesScreenshot {
+    void open();
 
+    void close();
 
+    String getTitle();
+}
+
+class ChromeDriver implements WebDriver, RemoteWebDriver, TakesScreenshot {
     @Override
     public void open() {
-
+         System.out.println("Chrome opened");
     }
-
     @Override
     public void close() {
-
+        System.out.println("Chrome closed");
     }
-
     @Override
     public String getTitle() {
-        return null;
+        String title = "google.com";
+        return title;
     }
-
     @Override
     public void navigate() {
+        System.out.println("Navigating to Onet.pl...");
+    }
+    @Override
+    public void getScreenshot() {
+        System.out.println("Saving screenshot of the webpage...");
+    }
+}
 
+class FirefoxDriver implements WebDriver, RemoteWebDriver, TakesScreenshot {
+    @Override
+    public void open() {
+        System.out.println("Firefox opened");
+    }
+    @Override
+    public void close() {
+        System.out.println("Firefox closed");
+    }
+    @Override
+    public String getTitle() {
+        String title = "Onet.pl";
+        return title;
+    }
+    @Override
+    public void navigate() {
+        System.out.println("Navigating to Wp.pl...");
     }
 
     @Override
     public void getScreenshot() {
+        System.out.println("Saving screenshot of the webpage...");
+    }
+}
+class SafariDriver implements WebDriver, RemoteWebDriver, TakesScreenshot {
+    @Override
+    public void open() {
+        System.out.println("Safari opened");
+    }
+    @Override
+    public void close() {
+        System.out.println("Safari closed");
+    }
+    @Override
+    public String getTitle() {
+        String title = "Wp.pl";
+        return title;
+    }
+    @Override
+    public void navigate() {
+        System.out.println("Navigating to interia.pl...");
+    }
 
+    @Override
+    public void getScreenshot() {
+        System.out.println("Saving screenshot of the webpage...");
     }
 }
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 public class WDInterface {
+    public static void main (String[] args){
+
+        WebDriver browserChrome = new ChromeDriver();
+        WebDriver browserFirefox = new FirefoxDriver();
+        WebDriver browserSafari = new SafariDriver();
+
+        WebDriver[] browsers = {new ChromeDriver(), new FirefoxDriver(), new SafariDriver()};
+
+        for(WebDriver CFS : browsers){
+            CFS.open();
+            System.out.println("Title is: " + CFS.getTitle());
+            CFS.navigate();
+            CFS.getScreenshot();
+            CFS.close();
+            System.out.println();
+        }
+
+
+
+    }
 }
